17315c9c515971c2cd599ef582f8931889ca47cd
Ivan Klymchuk <klymhuk.ivan@gmail.com>
2017-12-08 22:55:45 +0200

    update env_configuration

--- env_configuration.js
+++ env_configuration.js
@@ -1,43 +1,36 @@
 #!/usr/bin/env node
 
-new Promise(function(resolve, reject) {
-    
-      setTimeout(() => resolve(1), 1000); 
-    
-    }).then(function(result) {
-        var exec = require('child_process').exec;
-        var child = exec('npm install', function(err, stdout, stderr) {
-            console.log("npm install");
-            console.log(stdout);
-        });
-    
-    }).then(function(result) {
-        var exec = require('child_process').exec;
-        var child = exec('git --version', function(err, stdout, stderr) {
-            console.log("git --version");
-            console.log(stdout);
-        });
-
-    }).then(function(result) { 
-        var exec = require('child_process').exec;
-        var child = exec('npm -v', function(err, stdout, stderr) {
-            console.log("npm -v");
-            console.log(stdout);
-        });
-    
-    }).then(function(result) {
-    
-        var exec = require('child_process').exec;
-        var child = exec('node -v', function(err, stdout, stderr) {
-            console.log("node -v");
-            console.log(stdout);
-        });
-
-    }).then(function(result) {
-        var exec = require('child_process').exec;
-        var child = exec('mkdir config', function(err, stdout, stderr) {
-            console.log("config folder was created");
-            console.log(stdout);
-        });
-    
+    cmd('npm install')    
+    .then(function() {
+        cmd('git --version');
+    }).then(function() {
+        cmd('npm -v');
+    }).then(function() {
+        cmd('node -v');
+    }).then(function() {
+        cmd('mkdir config');
     });
+
+    function cmd(command) {
+        var promise = new Promise(function(resolve, reject) {
+            
+            setTimeout(function (){
+                
+                var exec = require('child_process').exec;
+                var child = exec(command, function(err, stdout, stderr) {
+                    console.log(command);
+                    console.log(stdout);
+                    if (err) {
+                        console.log(command.toUpperCase() + ' ERROR!');  
+                        console.log(err);   
+                        reject(err)                     
+                    } else {
+                        resolve(command)                                                   
+                    }
+                });
+                
+        }, 1000)
+    })
+    return promise;
+}
+
